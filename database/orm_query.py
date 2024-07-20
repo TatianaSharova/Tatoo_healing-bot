5d9381da98f78960784498aa9b28470ab9396443
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-07-20 22:29:33 +0400

    add orm func to add user to db

--- database/orm_query.py
+++ database/orm_query.py
@@ -2,18 +2,29 @@ from aiogram import types
 from sqlalchemy import delete, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database.models import Data
+from database.models import User
+
+
+async def orm_user(session: AsyncSession,
+                               data: dict, message: types.Message):
+    '''Создание подписки на уведомления.'''
+    obj = User(
+        user_id=message.from_user.id,
+        healing_method=data['healing_method']
+        )
+    session.add(obj)
+    await session.commit()
 
 
 async def orm_get_list_users(session: AsyncSession):
     '''Получить абсолютно все подписки, для админа.'''
-    query = select(Data)
+    query = select(User)
     result = await session.execute(query)
     return result.scalars().all()
 
 
 async def orm_delete_user(session: AsyncSession, user_id: int):
     '''Удалить пользователя из базы данных.'''
-    query = delete(Data).where(Data.user_id == user_id)
+    query = delete(User).where(User.user_id == user_id)
     await session.execute(query)
     await session.commit()
