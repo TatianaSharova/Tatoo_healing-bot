e8d6e9de85566853288ce76c0561a9d6b3cbb8fb
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-07-20 22:28:43 +0400

    add User  model

--- database/models.py
+++ database/models.py
@@ -4,9 +4,16 @@ from sqlalchemy import Float, Integer, String, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-# class Base(DeclarativeBase):
-#     pass
+class Base(DeclarativeBase):
+    pass
 
 
-# class Data(Base):
-#     pass
+class User(Base):
+    '''Модель для пользователя.'''
+    __tablename__ = 'user'
+
+    id: Mapped[int] = mapped_column(primary_key=True,
+                                    autoincrement=True)
+    user_id: Mapped[str] = mapped_column(Integer, nullable=False)
+    healing_method: Mapped[str] = mapped_column(String(8))
+    notify_counter: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
