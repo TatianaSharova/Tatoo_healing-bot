82ae5aebcf6b98bfac739003a798faa385a6a35f
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-01-10 11:32:50 +0400

    delete unuseful counting in user model

--- database/models.py
+++ database/models.py
@@ -1,12 +1,14 @@
 from typing import Optional
 
-from sqlalchemy import Integer, String, DateTime, func
+from sqlalchemy import DateTime, Integer, String, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
+    '''Базовая модель.'''
     created: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
-    updated: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+    updated: Mapped[DateTime] = mapped_column(DateTime, default=func.now(),
+                                              onupdate=func.now())
 
 
 class User(Base):
@@ -17,5 +19,5 @@ class User(Base):
                                     autoincrement=True)
     user_id: Mapped[int] = mapped_column(Integer, nullable=False)
     username: Mapped[str] = mapped_column(String, nullable=False)
-    healing_method: Mapped[Optional[str]] = mapped_column(String(8), nullable=True)
-    notify_counter: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    healing_method: Mapped[Optional[str]] = mapped_column(String(8),
+                                                          nullable=True)
